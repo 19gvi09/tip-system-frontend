b217329253769b1ca7b0420f08a6c268612fa881
Anton2-7 <60310157+Anton2-7@users.noreply.github.com>
2021-12-11 18:12:33 +0500

    добавлена_шапка_и_finance

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "lk" */ '../views/LK.vue')
-  }
+  },
+  {
+    path: '/finance',
+    name: 'Finance',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "finance" */ '../views/Finance.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Finance.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<FinHeader />
+</div>
+</template>
+
+<script>
+import FinHeader from '../components/Finance/FinHeader.vue'
+
+export default {
+    name: 'Finance',
+    components: {
+        FinHeader,
+    }
+}
+</script>
+
+<style lang = "scss" scoped>
+
+</style>
