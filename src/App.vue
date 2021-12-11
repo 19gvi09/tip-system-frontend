688c4986e178af0f1a09bf23a4e140c220f5d383
vladislav_glumov <19gvi09@gmail.com>
2021-12-12 02:23:55 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,11 @@ export default {
     components: {
         Header,
         Footer,
+    },
+    mounted() {
+        fetch("https://web.rbsuat.com/ab/rest/sbp/c2b/qr/status.do?userName=userName&password=password&mdOrder=04888d6f-7920-7531-8332-8de901efddd0&qrId=3946c0c02d1042f7b7e63cc0f1b52a95", {method: "POST"})
+            .then(res => res.json())
+            .then(res => console.log(res))
     }
 }
 </script>
